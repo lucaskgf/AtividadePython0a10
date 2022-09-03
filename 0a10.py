53566f36d8d6609b2a993c74bf4d50d12580621f
lucaskgf <luquinhaskgf@gmail.com>
2022-09-02 22:56:32 -0300

    first commit

---
+++ 0a10.py
@@ -0,0 +1,9 @@
+nota=float(input("Digite um número de 0 a 10: "))
+if nota >10:
+    print("Digite novamente...")
+while (nota>10) or (nota<0):
+    nota=float(input("informe um numero de 0 a 10: "))
+    if nota >10:
+        print("Digite novamente...")
+    
+
